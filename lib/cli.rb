7d9a353a9eec0b9d79745b73d57872374025b66b
Chenxi Wu <wuchenxi2013@gmail.com>
2016-11-14 20:10:10 +0100

    finalized cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,21 +9,23 @@ class FFs
     while true
      begin
       puts "----------------------"
-      @cur_story.print_summary
-      puts "----------------------"
-      @cur_author.print_bio
-      puts "----------------------"
-
       puts "A: new author"
       puts "S: new story"
-      puts "L: list author's stories/go to another story by the same author"
+      puts "L: list all stories / switch to another story by the same author"
       puts "D: keep current story"
+      puts "P: display current story anh / or author"
       puts "Q: quit"
       puts "----------------------"
 
       s=gets
       if s[0]=='q' || s[0]=='Q'
          break
+      elsif s[0]=='p' || s[0]=='P'         
+         puts "----------------------"
+         @cur_story.print_summary
+         puts "----------------------"
+         @cur_author.print_bio
+         puts "----------------------"
       elsif s[0]=='s' || s[0]=='S'
          puts "Enter id or url:"         
          @cur_story=Story.new(gets).get_summary
@@ -36,6 +38,10 @@ class FFs
          puts "Saved."
       elsif s[0]=='L'||s[0]=='l'
          @cur_author.list_stories
+         puts "----------------------"
+         puts "Enter new story number (0 for staying at the current story)"
+         n=gets.to_i
+         @cur_story=@cur_author.stories[i-1]
       end
 
       rescue => e
