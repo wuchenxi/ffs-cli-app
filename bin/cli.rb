17f36231e18bb3472ab354e5ec3d287c62df1ba0
Chenxi Wu <wuchenxi2013@gmail.com>
2016-11-16 16:30:09 +0100

    cli change

--- bin/cli.rb
+++ bin/cli.rb
@@ -3,6 +3,6 @@
 require_relative "../lib/environment.rb"
 
 #Story.new("https://www.fanfiction.net/s/12110896/11/Fortune-s-Might").savetext
-Story.new("465437").print.savetext
-Author.new("https://www.fanfiction.net/u/933974/").print.list_stories
-#FFs.new.cli
+#Story.new("465437").print.savetext
+#Author.new("https://www.fanfiction.net/u/933974/").print.list_stories
+FFs.new.cli
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,50 +1,49 @@
 class FFs
-  attr_accessor :cur_story, :cur_author
+  attr_accessor :cur
   def initialize
-     @cur_story=Story.new
-     @cur_author=Author.new
+     @cur=Scrapped_object.new
   end
   
   def cli
     while true
      begin
-      puts "A: new author  S: new story  Q: quit"
-      puts "L: list all stories/switch to another story by the same author"
-      puts "D: download current story  P: display current story and/or author"
-      puts "----------------------"
+      puts ""
+      if @cur.is_a?(Story) then puts "Story: "+cur.title end
+      if @cur.is_a?(Author) then puts "Author: "+cur.name end
+
+      puts "S: new story A: new author P: display info about current story or author"
+      puts "Q: quit D: download current story C: go to the author of current story"
+      puts "L: list all stories by the current author" 
+      puts "N: select story by current author by index in the list"
 
       s=gets
       if s[0]=='q' || s[0]=='Q'
          break
-      elsif s[0]=='p' || s[0]=='P'         
-         puts "Story-----------------"
-         @cur_story.print_summary
-         puts "Author----------------"
-         @cur_author.print_bio
-         puts "----------------------"
+      elsif s[0]=='p' || s[0]=='P'      
+         @cur.print
       elsif s[0]=='s' || s[0]=='S'
          puts "Enter id or url:"         
-         @cur_story=Story.new(gets).get_summary
-         @cur_author=@cur_story.author
+         @cur=Story.new(gets).get_summary
       elsif s[0]=='a' || s[0]=='A'
          puts "Enter id or url:"
-         @cur_author=Author.new(gets).get_bio
+         @cur=Author.new(gets).get_bio
       elsif s[0]=='D' || s[0]=='d'
-         @cur_story.savetext
-         puts "Saved."
+         if @cur.is_a?(Story) then @cur.savetext; puts "Saved." end
       elsif s[0]=='L'||s[0]=='l'
-         @cur_author.list_stories
-         puts "----------------------"
-         puts "Enter new story number (0 for staying at the current story)"
-         n=gets.to_i
-         if n>0 then @cur_story=@cur_author.stories[n-1] end
+         if @cur.is_a?(Author) then @cur.list_stories end
+      elsif s[0]=='N'||s[0]=='n'
+         if @cur.is_a?(Author)
+            puts "Enter story index:"
+            n=gets.to_i; @cur=@cur.stories[n-1] 
+         end
+      elsif s[0]=='C'||s[0]=='c'
+         if @cur.is_a?(Story) then @cur=cur.author.get_bio end
       end
 
       rescue => e
          puts e.message
-         #reset
-         @cur_story=Story.new
-         @cur_author=Author.new
+         #reset     
+         @cur=Scrapped_object.new
      end
     end
   end
